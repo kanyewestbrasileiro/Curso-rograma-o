d04c746fdc3163dfc82e39e61702e4361983a006
kanyewestbrasileiro <isaque123456789oi@gmail.com>
2025-05-19 18:44:58 -0300

    Add files via upload

---
+++ lista coluna e linha.py
@@ -0,0 +1,18 @@
+lista = [3,2,4]
+matriz = [
+    [3,1,5],
+    [54,62,6]
+
+]
+for l in range(len(matriz)):
+    for c in range(matriz):
+        print(matriz[l][c])
+
+for linha in matriz:
+    for valor in linha:
+        print(valor)
+
+
+
+
+link https://drive.google.com/drive/folders/1ehFFce5RBHERFQ2uvHIa2UNtSuCo1wSE
